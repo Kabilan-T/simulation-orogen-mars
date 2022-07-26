beffd13cb81f314c76e4f848b6864f06ab946aeb
Raul Dominguez <raul.dominguez@dfki.de>
2022-07-26 15:29:04 +0200

    [test_tmds] Apply the configuration file must be done before configure is called.

--- scripts/tmds_debug.rb
+++ scripts/tmds_debug.rb
@@ -9,13 +9,13 @@ Orocos.run 'tether' do
     mars = TaskContext.get 'mars'
     tether = TaskContext.get 'tether'
 
+    Orocos.apply_conf_file(mars,'/opt/workspace/bundles/corobx_simulation/config/orogen/mars::Task.yml',['default','rappelling_maguez'])
     mars.configure
     mars.start
 
     tether.configure
     tether.start
   
-    Orocos.apply_conf_file(mars,'/opt/workspace/bundles/corobx_simulation/config/orogen/mars::Task.yml',['default','rappelling_maguez'])
 
     Readline::readline("Press ENTER to quit") do
 
